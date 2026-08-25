9ded3e2ed9c27fa95e45517516b44e8b24a2f1a3
mengmenghuo <2485501626@qq.com>
2026-08-25 10:30:30 +0800

    26.1 fix

--- src/client/java/com/mmh1146/menuremove/client/mixin/PauseScreenMixin.java
+++ src/client/java/com/mmh1146/menuremove/client/mixin/PauseScreenMixin.java
@@ -1,45 +1,24 @@
 package com.mmh1146.menuremove.client.mixin;
 
 import com.mmh1146.menuremove.client.config.GameMenuConfig;
-import net.minecraft.client.gui.components.AbstractWidget;
-import net.minecraft.client.gui.layouts.LinearLayout;
-import net.minecraft.client.gui.layouts.LayoutElement;
 import net.minecraft.client.gui.screens.PauseScreen;
-import net.minecraft.network.chat.ComponentContents;
-import net.minecraft.network.chat.contents.TranslatableContents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PauseScreen.class)
 public class PauseScreenMixin {
+	// In 26.1 this method only adds the "Give Feedback" and "Report Bugs" buttons
+	@Inject(method = "addFeedbackButtons", at = @At("HEAD"), cancellable = true)
+	private static void removeFeedbackButtons(CallbackInfo ci) {
+		if (GameMenuConfig.isEnabled()) {
+			ci.cancel();
+		}
+	}
+
 	@Inject(method = "createPauseMenu", at = @At("HEAD"))
 	private void reloadConfig(CallbackInfo ci) {
 		GameMenuConfig.load();
-	}
-
-	@Redirect(
-		method = "createPauseMenu",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/client/gui/layouts/LinearLayout;addChild(Lnet/minecraft/client/gui/layouts/LayoutElement;)Lnet/minecraft/client/gui/layouts/LayoutElement;"
-		)
-	)
-	private LayoutElement filterBugFeedbackButtons(LinearLayout layout, LayoutElement element) {
-		if (!GameMenuConfig.isEnabled()) {
-			return layout.addChild(element);
-		}
-		if (element instanceof AbstractWidget widget) {
-			ComponentContents contents = widget.getMessage().getContents();
-			if (contents instanceof TranslatableContents translatable) {
-				String key = translatable.getKey();
-				if ("menu.reportBugs".equals(key) || "menu.sendFeedback".equals(key)) {
-					return null;
-				}
-			}
-		}
-		return layout.addChild(element);
 	}
 }
